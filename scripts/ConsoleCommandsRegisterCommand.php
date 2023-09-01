e48be42ce81ed2c617d7b1e7f2836a4e891703b0
enjoys <deadly.info@gmail.com>
2023-09-01 16:12:12 +0300

    refactor commands

--- scripts/ConsoleCommandsRegisterCommand.php
+++ scripts/ConsoleCommandsRegisterCommand.php
@@ -2,44 +2,18 @@
 
 namespace EnjoysCMS\Module\Sitemap\Composer\Scripts;
 
-use Enjoys\Config\Config;
-use EnjoysCMS\Core\Console\Utils\CommandsManage;
-use EnjoysCMS\Module\Catalog\Command\CurrencyRate;
+use EnjoysCMS\Core\Composer\Scripts\RegisterCommandsToConsoleCommand;
 use EnjoysCMS\Module\Sitemap\Command\Generate;
 use EnjoysCMS\Module\Sitemap\Command\Status;
-use Exception;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
-class ConsoleCommandsRegisterCommand extends Command
+class ConsoleCommandsRegisterCommand extends RegisterCommandsToConsoleCommand
 {
 
-    private array $commands = [
+    protected string $moduleName = 'Sitemap';
+    protected array $commands = [
         Generate::class => [],
         Status::class => [],
     ];
 
-    /**
-     * @throws Exception
-     */
-    public function execute(InputInterface $input, OutputInterface $output): int
-    {
-        if ($this->commands === []){
-            return Command::SUCCESS;
-        }
-        $container = include __DIR__ . '/../../../bootstrap.php';
-        $commandManage = new CommandsManage(config: $container->get(Config::class));
-        $registeredCommands = $commandManage->registerCommands($this->commands);
-        $changed = false;
-        $output->writeln('<info>Register console commands [Sitemap]</info>');
-        foreach ($registeredCommands as $command) {
-            $output->writeln(sprintf(' <options=bold>- %s</></info>', $command));
-            $changed = true;
-        }
-        if ($changed) {
-            $commandManage->save();
-        }
-        return Command::SUCCESS;
-    }
+
 }
